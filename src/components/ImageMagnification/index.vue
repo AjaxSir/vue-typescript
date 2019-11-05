b70bb352bf2feef67adf77672c53f4915133f6f6
金丽虹 <794473046@qq.com>
2019-11-05 10:02:28 +0800

    新增车位管理页面

---
+++ src/components/ImageMagnification/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="centerDialogVisible" class="big-img-box">
+    <p class="big-title">{{bigTitle}}</p>
+    <img class="big-image" :src="bigImg" alt="">
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  props: {
+    bigImg: {
+      type: String,
+      default: ''
+    },
+    bigTitle: {
+      type: String,
+      default: ''
+    },
+    centerDialogVisible: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style  rel="stylesheet/scss" lang="scss" scoped>
+.big-img-box {
+  width: 260px;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  text-align: center;
+  padding: 0 25px 40px 25px;
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.14);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-left: -130px;
+  margin-top: -160px;
+  background: #fff;
+  z-index: 999;
+}
+.big-title {
+  margin: 10px 0;
+  font-size: 18px;
+}
+.big-image {
+  width: 200px;
+  vertical-align: middle;
+}
+</style>
