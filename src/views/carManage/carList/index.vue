f29587d8dfe4260b54c63b9bba26dff701077cd2
金丽虹 <794473046@qq.com>
2019-12-10 11:19:39 +0800

    优化鼠标移动到抓拍图片上时，大图显示

--- src/views/carManage/carList/index.vue
+++ src/views/carManage/carList/index.vue
@@ -492,7 +492,7 @@
         <el-button type="primary" @click="detailDialogVisible = false">确 定</el-button>
       </span>-->
     </el-dialog>
-    <ImageMagni :centerDialogVisible="imgVisible" bigTitle="抓拍图片" :bigImg="bigImg" />
+    <!-- <ImageMagni :centerDialogVisible="imgVisible" bigTitle="抓拍图片" :bigImg="bigImg" /> -->
   </div>
 </template>
 
